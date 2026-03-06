ee4a768f93869be311c8cc423297501ec6edb416
Gerardo Siblesz <gsiblesz@correo.unimet.edu.ve>
2026-03-06 14:34:57 -0400

    Update menu link URL

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbyNcOB0zx2J4z1oPKnnWRsfDx6bUvJf7VpUQRU7PfY0Epr3zz31pEcWYVm8aWWPzNAq/exec"; // despliegue Apps Script produccion diaria
-const MENU_LINK = "https://menu-almacen.vercel.app/"; // URL del menú principal
+const MENU_LINK = "https://menu-ten-sigma.vercel.app/"; // URL del menú principal
 
 const form = document.getElementById("production-form");
 const statusEl = document.getElementById("status");
